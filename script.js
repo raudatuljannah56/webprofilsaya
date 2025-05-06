f8a293e99ddb16736ecad3bf6469dee4a552bfcd
raudatuljannah56 <rraudatuljannahhh56@gmail.com>
2025-05-06 08:23:49 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleBiodata() {
-    const sections = document.querySelectorAll("main section");
-    sections.forEach(sec => {
-        sec.style.display = sec.style.display === "none" ? "block" : "none";
-    });
-}
